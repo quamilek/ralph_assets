8fcbe75ced3edbe20a5b3a3b83ca70618534ba12
Kamil Warguła <kwargula@gmail.com>
2014-07-15 14:35:26 +0200

    Add test report - categor->model->status tree

--- src/ralph_assets/tests/functional/tests_reports.py
+++ src/ralph_assets/tests/functional/tests_reports.py
@@ -10,7 +10,7 @@ from django.core.urlresolvers import reverse
 from django.test import TestCase
 
 from ralph.ui.tests.global_utils import login_as_su
-from ralph_assets.models_assets import AssetCategory
+from ralph_assets.models_assets import AssetCategory, AssetStatus
 from ralph_assets.tests.utils.assets import BOAssetFactory
 from ralph_assets.tests.utils.assets import (
     AssetModelFactory,
@@ -48,17 +48,60 @@ class TestReportCategoryTreeView(TestCase):
             category=AssetCategory.objects.get(name="Shredder")
         )
 
-    def _create_assets(self):
+    def _create_assets_without_status(self):
         [BOAssetFactory(**{'model': self.keyboard_model}) for _ in xrange(6)]
         [BOAssetFactory(**{'model': self.mouse_model}) for _ in xrange(2)]
         [BOAssetFactory(**{'model': self.pendrive_model}) for _ in xrange(2)]
 
-        [BOAssetFactory(**{'model': self.model_monitor}) for _ in xrange(2)]
-        [BOAssetFactory(**{'model': self.navigation_model}) for _ in xrange(2)]
-        [BOAssetFactory(**{'model': self.scanner_model}) for _ in xrange(3)]
-        [BOAssetFactory(**{'model': self.shredder_model}) for _ in xrange(3)]
+    def _create_assets_with_status(self):
+        AS = AssetStatus
+        [
+            BOAssetFactory(**{'model': self.keyboard_model, 'status': status.id})  # noqa
+            for status in [AS.new, AS.new, AS.used, AS.used, AS.loan, AS.loan]
+        ]
+        [
+            BOAssetFactory(**{'model': self.mouse_model, 'status': status.id})
+            for status in [AS.new, AS.used]
+        ]
+        [
+            BOAssetFactory(**{'model': self.pendrive_model, 'status': status.id})  # noqa
+            for status in [AS.new, AS.used]
+        ]
+
+    def test_category_model_status_tree(self):
+        self._create_assets_with_status()
+        url = reverse(
+            'report',
+            kwargs={'mode': 'back_office', 'slug': 'category-model-status'},
+        )
+        response = self.client.get(url, follow=True)
+        report_dict = response.context_data.get('report_dict')
+        back_office_dict = report_dict['BACK OFFICE']
+        accesories_dict = back_office_dict['children']['ACCESSORIES']
+
+        self.assertEqual(back_office_dict['count'], 10)
+        self.assertEqual(accesories_dict['count'], 10)
+
+        accesories_children = accesories_dict['children']
+
+        keyboard_children = accesories_children['Keyboard']['children']
+        self.assertEqual(keyboard_children['new']['count'], 2)
+        self.assertEqual(keyboard_children['used']['count'], 2)
+        self.assertEqual(keyboard_children['loan']['count'], 2)
+        self.assertEqual(keyboard_children['damaged']['count'], 0)
+
+        mouse_children = accesories_children['Mouse']['children']
+        self.assertEqual(mouse_children['new']['count'], 1)
+        self.assertEqual(mouse_children['used']['count'], 1)
+        self.assertEqual(mouse_children['loan']['count'], 0)
+
+        pendrive_children = accesories_children['Pendrive']['children']
+        self.assertEqual(pendrive_children['new']['count'], 1)
+        self.assertEqual(pendrive_children['used']['count'], 1)
+        self.assertEqual(pendrive_children['loan']['count'], 0)
 
     def test_category_model_tree(self):
+        self._create_assets_without_status()
         url = reverse(
             'report', kwargs={'mode': 'back_office', 'slug': 'category-model'},
         )
